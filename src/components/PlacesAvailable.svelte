afd73bb853742b44edfd49c884a3fcd72fe96d50
baezdev <baezdev@gmail.com>
2023-08-07 23:58:28 -0600

    add header component

---
+++ src/components/PlacesAvailable.svelte
@@ -0,0 +1,8 @@
+<div class="flex items-center gap-3 bg-secondary w-max px-4 rounded-md py-3 mb-5">
+  <span>👙</span>
+  <div class="flex flex-col">
+    <span class="text-[10px] uppercase leading-none">Lugares</span>
+    <p class="text-sm font-semibold leading-none">Disponibles</p>
+  </div>
+  <span class="text-lg font-semibold">30</span>
+</div>
