c75778999d4df4a79884b2ddf8146352120b3f1c
baezdev <baezdev@gmail.com>
2023-08-11 00:36:55 -0600

    add itinerary components

---
+++ src/components/ActivitiesList.svelte
@@ -0,0 +1,17 @@
+<script>
+  import itinerary from "../data/itinerary.json";
+  import Activity from "./Activity.svelte";
+</script>
+
+<article>
+  {#each itinerary as { day, activities }}
+    <p class="font-semibold text-xl md:text-3xl mb-7 md:ml-32">{day}</p>
+    <ul
+      class="mx-auto w-11/12 md:w-max flex flex-col gap-4 border-l-[3px] border-dashed border-gray-500 border-opacity-40 mb-16"
+    >
+      {#each activities as act}
+        <Activity {...act} />
+      {/each}
+    </ul>
+  {/each}
+</article>
---
+++ src/components/Activity.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let hrs
+  export let activity
+  export let image
+</script>
+
+<li
+  class="flex w-full md:w-96 justify-between pl-6 before:content-[''] before:bg-[url(/images/check.svg)] before:w-6 before:h-6 before:absolute relative before:bg-cover before:-left-[13px] before:-top-1 gap-2 md:gap-3"
+>
+  <div>
+    <time class="text-lg text-gray-600 font-medium">{hrs}</time>
+    <p class="text-xl md:text-2xl">{activity}</p>
+  </div>
+  <picture>
+    <img
+      src={image}
+      alt={activity}
+      class="w-20 md:w-24 aspect-square rounded-2xl"
+      loading="lazy"
+    />
+  </picture>
+</li>
