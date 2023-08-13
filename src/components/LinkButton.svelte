6f4494cd6976a59d2bc570b237a95f27b8c11eb7
baezdev <baezdev@gmail.com>
2023-08-12 19:33:44 -0600

    change styles

--- src/components/LinkButton.svelte
+++ src/components/LinkButton.svelte
@@ -1,14 +1,13 @@
 <script>
   import { twMerge } from 'tailwind-merge'
+  import { baseStyle } from '../styles/button';
 
   export let href
   export let variant = 'primary'
 
-  const baseStyle = "relative z-10 px-3 md:py-[5px] py-3 md:text-base text-3xl font-semibold rounded-md capitalize transition-all duration-200 w-fit mx-auto before:content-[''] before:w-full before:h-full before:block before:absolute before:top-0 before:rounded-md before:-z-10 before:left-0 hover:before:scale-110 before:transition-all before:duration-200"
-
   const getVariantStyles = () => {
     return variant === 'primary'
-      ? 'text-white before:bg-primary text text-xl px-7 py-4 md:py-3'
+      ? 'text-white before:bg-primary text-xl px-7 py-4 md:py-3'
       : 'text-primary before:bg-primary before:bg-opacity-20 md:text-base text-3xl'
   }
 
