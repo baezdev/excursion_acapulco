065436744373f4be58ed51db16bcc7e4354902a4
baezdev <baezdev@gmail.com>
2023-08-28 14:07:23 -0600

    add type button

--- src/components/ui/LinkButton.svelte
+++ src/components/ui/LinkButton.svelte
@@ -1,20 +1,31 @@
 <script>
-  import { twMerge } from 'tailwind-merge'
-  import { baseStyle } from '../../styles/button';
+  import { twMerge } from "tailwind-merge";
+  import { baseStyle } from "../../styles/button";
 
-  export let href
-  export let variant = 'primary'
+  export let href = "";
+  export let variant = "primary";
+  export let type = "link";
+  export let disabled = false;
 
   const getVariantStyles = () => {
-    return variant === 'primary'
-      ? 'text-white before:bg-primary text-xl px-7 py-4 md:py-3'
-      : 'text-primary before:bg-primary before:bg-opacity-20 md:text-base text-3xl'
-  }
+    return variant === "primary"
+      ? "text-white before:bg-primary text-xl px-7 py-4 md:py-3 mx-0"
+      : "text-primary before:bg-primary before:bg-opacity-20 md:text-base text-3xl";
+  };
 
-  const variantButton = getVariantStyles()
+  const variantButton = getVariantStyles();
 </script>
-<a
-  href={href}
-  class={twMerge(baseStyle, variantButton)}>
-  <slot />
-</a>
+
+{#if type === "link"}
+  <a {href} class={twMerge(baseStyle, variantButton)}>
+    <slot />
+  </a>
+{:else}
+  <button
+    class={twMerge(baseStyle, variantButton, disabled === true && 'before:invert hover:before:scale-100')}
+    {href}
+    {disabled} 
+  >
+    <slot />
+  </button>
+{/if}
