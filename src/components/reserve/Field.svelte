f380b32b762e7639878602716c72f185a40798d5
baezdev <baezdev@gmail.com>
2023-08-28 14:04:08 -0600

    add form componets

---
+++ src/components/reserve/Field.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let fieldValue;
+  export let name;
+  export let type = "text";
+  export let placeholder;
+  export let label;
+
+  const fieldStyle =
+    "py-1 font-semibold appearance-none focus:outline-none text-font placeholder:text-gray-400";
+</script>
+
+<div
+  class="flex flex-col border-b-2 border-b-gray-400 border-opacity-30 focus-within:border-primary focus-within:border-opacity-100 group"
+>
+  <label
+    for={name}
+    class="font-semibold text-font group-focus-within:text-primary"
+    >{label}</label
+  >
+  {#if type === "text"}
+    <input
+      bind:value={fieldValue}
+      type="text"
+      class={fieldStyle}
+      {placeholder}
+      {name}
+    />
+  {:else if type === "number"}
+    <input
+      bind:value={fieldValue}
+      type="number"
+      class={fieldStyle}
+      {placeholder}
+      {name}
+    />
+  {/if}
+</div>
---
+++ src/components/reserve/ReserveForm.svelte
@@ -0,0 +1,52 @@
+<script>
+  import LinkButton from "../ui/LinkButton.svelte";
+  import Field from "./Field.svelte";
+
+  let name = "";
+  let phone = "";
+  let termsWereAccepted = false;
+
+  const handleSubmit = (e) => {
+    if (!termsWereAccepted) {
+      console.log("Acepta hdp");
+      return;
+    }
+
+    console.log({ name, phone, termsWereAccepted });
+  };
+</script>
+
+<form on:submit|preventDefault={handleSubmit} class="flex-1 px-5">
+  <p>Llena los siguientes datos y nosotros te contactamos</p>
+  <div class="flex flex-col gap-5">
+    <Field
+      bind:fieldValue={name}
+      label="Nombre"
+      name="name"
+      placeholder="Ingrese su nombre"
+    />
+    <Field
+      bind:fieldValue={phone}
+      label="Telefono"
+      name="phone"
+      placeholder="Ingrese su numero de telefono"
+      type="number"
+    />
+    <label class="flex items-center">
+      <input
+        type="checkbox"
+        class="w-5 h-5 text-white transition duration-200 ease-in-out border-2 border-gray-400 rounded-full appearance-none accent-primary checked:bg-primary border-opacity-30"
+        bind:checked={termsWereAccepted}
+      />
+      <span class="ml-2 text-font">Acepto los términos y condiciones</span>
+    </label>
+    <div class="flex justify-end w-full mt-5">
+      <LinkButton
+        type="button"
+        disabled={!termsWereAccepted}
+      >
+        Quiero Reservar</LinkButton
+      >
+    </div>
+  </div>
+</form>
