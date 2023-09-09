add2ed782e6bdf3b4f8d9d3641025d524db38a05
baezdev <baezdev@gmail.com>
2023-09-09 01:05:55 -0600

    add component for checkbox

---
+++ src/components/reserve/TermsCheckbox.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let checked;
+</script>
+
+<label class="flex items-center">
+  <input
+    type="checkbox"
+    class="w-5 h-5 text-white transition duration-200 ease-in-out border-2 border-gray-400 rounded-full appearance-none accent-primary checked:bg-primary border-opacity-30"
+    bind:checked
+  />
+  <span class="ml-2 text-font">Acepto los términos y condiciones</span>
+</label>
