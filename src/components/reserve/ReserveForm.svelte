2421fd6d8f2cf5be1b5b099876247b835732c187
baezdev <baezdev@gmail.com>
2023-09-09 01:08:18 -0600

    add simulator of cotization

--- src/components/reserve/ReserveForm.svelte
+++ src/components/reserve/ReserveForm.svelte
@@ -1,32 +1,50 @@
 <script>
+  import {
+    calculatePercentage,
+    calculateTotal,
+    handleSumTotals,
+  } from "../../helpers/operations";
   import { validateForm } from "../../helpers/validateForm";
   import LinkButton from "../ui/LinkButton.svelte";
   import Field from "./Field.svelte";
-  import Swal from "sweetalert2";
+  import TermsCheckbox from "./TermsCheckbox.svelte";
 
   let name = "";
   let phone = "";
-  let places = 0;
+  let rooms = [];
   let termsWereAccepted = false;
   let errors = {};
+  let total = 0;
+
+  const handleAddNewRoom = () => {
+    //TODO evitar que se agreguen mas de 3 habitaciones
+    rooms = [
+      ...rooms,
+      {
+        id: rooms.length + 1,
+        persons: "",
+        childrens: "",
+        total: 0,
+      },
+    ];
+  };
+
+  const handleDeleteRoom = (id) => {
+    rooms = rooms.filter((room) => room.id !== id);
+    total = calculatePercentage(handleSumTotals(rooms));
+  };
 
   const handleSubmit = () => {
-    errors = validateForm({ name, phone, places, palcesAvailable: 45 });
+    errors = validateForm({ name, phone, palcesAvailable: 45, rooms });
 
     if (Object.keys(errors).length > 0) {
       return;
     }
-
-    /* Swal.fire({
-      icon: 'success',
-      title: 'Listo',
-      text: 'Ya sabemos que quieres ir a Acapulco, te contactaremos',
-    }) */
-
-    window.open(
+    console.log('listo')
+    /* window.open(
       "https://api.whatsapp.com/send?phone=525631498641&text=Hola%Soy%Angel%y%quiero%ir%a%Acapulco",
       "_blank"
-    );
+    ); */
   };
 </script>
 
@@ -35,46 +53,100 @@
     Llena los siguientes datos y nosotros te contactamos
   </p>
   <div class="flex flex-col gap-5">
-    <Field
-      bind:fieldValue={name}
-      label="Nombre"
-      name="name"
-      placeholder="Ingrese su nombre"
-      error={errors.name}
-    />
-    <Field
-      bind:fieldValue={phone}
-      label="Telefono"
-      name="phone"
-      placeholder="Ingrese su numero de telefono"
-      type="number"
-      error={errors.phone}
-    />
-    <div class="flex items-center justify-between gap-5">
+    <div class="flex items-start justify-between gap-5">
       <Field
-        bind:fieldValue={places}
-        label="# de Lugares"
-        name="places"
-        placeholder="Cuantos lugares quieres reservar"
+        bind:fieldValue={name}
+        label="Nombre"
+        name="name"
+        placeholder="Ingrese su nombre"
+        error={errors.name}
+      />
+      <Field
+        bind:fieldValue={phone}
+        label="Telefono"
+        name="phone"
+        placeholder="Ingrese su numero de telefono"
         type="number"
-        error={errors.places}
+        error={errors.phone}
       />
-      <p class="text-xl font-bold">
-        ¡<span class="text-2xl text-primary">45</span> lugares disponibles!
-      </p>
     </div>
-    <label class="flex items-center">
-      <input
-        type="checkbox"
-        class="w-5 h-5 text-white transition duration-200 ease-in-out border-2 border-gray-400 rounded-full appearance-none accent-primary checked:bg-primary border-opacity-30"
-        bind:checked={termsWereAccepted}
-      />
-      <span class="ml-2 text-font">Acepto los términos y condiciones</span>
-    </label>
+    <div>
+      <div class="flex items-center justify-between">
+        <span class="text-xl font-bold">
+          {rooms.length}
+          {rooms.length !== 1
+            ? "Habitaciones agregadas"
+            : "Habitacion agregada"}
+        </span>
+        <LinkButton
+          variant="secondary"
+          type="button"
+          on:click={handleAddNewRoom}
+        >
+          Agregar habitacion
+        </LinkButton>
+      </div>
+      <div class="flex flex-col gap-3 mt-5">
+        {#each rooms as room}
+          <div
+            class="flex place-items-start gap-5 animate-fade animate-once animate-ease-in-out"
+          >
+            <Field
+              bind:fieldValue={room.persons}
+              label="# de personas"
+              name={`persons${room.id}`}
+              placeholder="Maximo 4"
+              type="number"
+              error={errors[`person${room.id}`]}
+              on:input={({ target }) => {
+                room.total = calculateTotal(target.value);
+                total = calculatePercentage(handleSumTotals(rooms));
+              }}
+            />
+            <Field
+              bind:fieldValue={room.childrens}
+              label="# de niños"
+              name={`childrens${room.id}`}
+              placeholder="Habra niños?"
+              type="number"
+              error={errors[`children${room.id}`]}
+              on:input={({ target }) => {
+                room.total = calculateTotal(room.persons, target.value);
+                total = calculatePercentage(handleSumTotals(rooms));
+              }}
+            />
+            <span class="flex-1 font-medium self-center"
+              >Total: <span class="text-xl font-semibold"
+                >{calculateTotal(room.persons, room.childrens)}</span
+              ></span
+            >
+            <button
+              type="button"
+              class="w-8"
+              on:click={handleDeleteRoom(room.id)}
+            >
+              <img
+                src="/icons/delete.svg"
+                alt="icono de eliminar"
+                class="w-full"
+              />
+            </button>
+          </div>
+        {/each}
+      </div>
+    </div>
+    {#if total > 0}
+      <p class="text-2xl font-semibold my-2">
+        El pago para reservar es de: $<span
+          class="text-3xl font-bold text-primary">{total}</span
+        >
+      </p>
+    {/if}
+    <TermsCheckbox bind:checked={termsWereAccepted} />
     <div class="flex justify-end w-full mt-5">
-      <LinkButton type="button" disabled={!termsWereAccepted}>
-        Quiero Reservar</LinkButton
-      >
+      <LinkButton type="button" disabled={!termsWereAccepted} role="submit">
+        Quiero Reservar
+      </LinkButton>
     </div>
   </div>
 </form>
