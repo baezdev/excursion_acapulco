d6e631e9c8c01f22b2386fcc08260d158bbefce1
baezdev <baezdev@gmail.com>
2023-10-18 01:36:20 -0600

    fix: remove rooms validate

--- src/helpers/validateForm.js
+++ src/helpers/validateForm.js
@@ -1,6 +1,6 @@
 import { NAME_REGEX, PHONE_REGEX } from '../constants/regex'
 
-export const validateForm = ({ name, phone, palcesAvailable, rooms }) => {
+export const validateForm = ({ name, phone, palcesAvailable }) => {
   const errors = {}
 
   //name validation
@@ -16,40 +16,12 @@ export const validateForm = ({ name, phone, palcesAvailable, rooms }) => {
 
   //phone validation
   if (phone === "") {
-    errors.phone = "El numero de telefono es requerido"
+    errors.phone = "El número de teléfono es requerido"
   }
 
   else if (!PHONE_REGEX.test(phone)) {
     errors.phone = "Deben ser 10 digitos"
   }
-
-  /* //places validation
-  if (places === 0) {
-    errors.places = "El numero de lugares debe ser mayor a 0"
-  }
-
-  else if (places > palcesAvailable) {
-    errors.places = `Lo siento solo hay ${palcesAvailable} lugares disponibles`
-  } */
-
-  //rooms validation one for one
-  rooms?.forEach((room) => {
-    if (room.persons === '') {
-      errors[`person${room.id}`] = 'Campo requerido'
-    }
-
-    else if (room.persons === 0) {
-      errors[`person${room.id}`] = 'No puede ser 0'
-    }
-
-    else if (room.persons < 2 || room.persons > 4 ) {
-      errors[`person${room.id}`] = '2, 3 o 4 personas'
-    }
-
-    if (room.childrens === 0) {
-      errors[`children${room.id}`] = 'No puede ser 0'
-    }
-  })
 
   return errors
 }
